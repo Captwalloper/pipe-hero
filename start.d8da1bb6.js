cc21147892f6e06b945cf6c977867001df27a222
Connor McCoy <comccoy@cfl.rr.com>
2025-01-06 23:52:12 -0500

    Updates

---
+++ start.d8da1bb6.js
@@ -0,0 +1,21 @@
+function e(e,t,r,o){Object.defineProperty(e,t,{get:r,set:o,enumerable:!0,configurable:!0})}var t=globalThis.parcelRequirec8fd,r=t.register;r("3ukco",function(r,o){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),e(r.exports,"default",()=>t("bMXg0").default),t("bMXg0")}),r("bMXg0",function(r,o){e(r.exports,"default",()=>l);var n=t("ayMG0"),i=t("4dK5w"),l=()=>(0,n.jsxs)(i.Start,{children:[(0,n.jsx)(i.Title,{children:"Pipe Hero"}),(0,n.jsx)(i.Prompt,{children:"Press any Key to Start!"})]})}),r("4dK5w",function(r,o){e(r.exports,"Start",()=>i),e(r.exports,"Title",()=>l),e(r.exports,"Prompt",()=>s);var n=t("lmFrL");let i=(0,n.styled).div`
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`,l=(0,n.styled).h1`
+  margin-bottom: 1em;
+
+  font-size: 108px;
+  font-weight: 700;
+  letter-spacing: -3px;
+  text-transform: uppercase;
+  color: #fff;
+`,s=(0,n.styled).span`
+  font-size: 54px;
+  font-weight: 700;
+  color: var(--brand);
+`});
+//# sourceMappingURL=start.d8da1bb6.js.map
